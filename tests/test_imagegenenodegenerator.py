be62045a8413fa870655c8666bf1e1a51da11dce
Chris Churas <churas.camera@gmail.com>
2023-04-28 17:23:15 -0700

    started fixing tests

--- tests/test_imagegenenodegenerator.py
+++ tests/test_imagegenenodegenerator.py
@@ -25,25 +25,25 @@ class TestAPMSGeneNodeAttributeGenerator(unittest.TestCase):
     def tearDown(self):
         """Tear down test fixtures, if any."""
 
-    def test_get_antibody_list(self):
-        gen = ImageGeneNodeAttributeGenerator(antibody_list='foo')
-        self.assertEqual('foo', gen.get_antibody_list())
+    def test_get_unique_list(self):
+        gen = ImageGeneNodeAttributeGenerator(unique_list='foo')
+        self.assertEqual('foo', gen.get_unique_list())
 
     def test_get_image_antibodies_from_csvfile(self):
         try:
             # Test case when csvfile is None
-            result = ImageGeneNodeAttributeGenerator.get_image_antibodies_from_csvfile()
+            result = ImageGeneNodeAttributeGenerator.get_unique_list_from_csvfile()
             self.fail('Expected exception')
         except CellMapsDownloaderError as ce:
             self.assertEqual('csvfile is None', str(ce))
 
         # Test case when file is not found
         with self.assertRaises(FileNotFoundError):
-            ImageGeneNodeAttributeGenerator.get_image_antibodies_from_csvfile('non_existent_file.csv')
+            ImageGeneNodeAttributeGenerator.get_unique_list_from_csvfile('non_existent_file.csv')
 
         # Test case when csvfile is empty
         with patch('builtins.open', mock_open(read_data='')):
-            result = ImageGeneNodeAttributeGenerator.get_image_antibodies_from_csvfile('test.csv')
+            result = ImageGeneNodeAttributeGenerator.get_unique_list_from_csvfile('test.csv')
             self.assertEqual(result, [])
 
         # Test case when csvfile has data
@@ -52,7 +52,7 @@ class TestAPMSGeneNodeAttributeGenerator(unittest.TestCase):
                    'Brain,CA1,1\nDEF,ENSG00000173672,GAD2,Brain,CA2,' \
                    '2\nGHI,ENSG00000172137,DLG4,Brain,CA3,3\n'
         with patch('builtins.open', mock_open(read_data=csv_data)):
-            result = ImageGeneNodeAttributeGenerator.get_image_antibodies_from_csvfile('test.csv')
+            result = ImageGeneNodeAttributeGenerator.get_unique_list_from_csvfile('test.csv')
             expected_result = [{'antibody': 'ABC',
                                 'ensembl_ids': 'ENSG00000171921',
                                 'gene_names': 'CDK5', 'atlas_name': 'Brain',
@@ -66,32 +66,4 @@ class TestAPMSGeneNodeAttributeGenerator(unittest.TestCase):
                                 'gene_names': 'DLG4', 'atlas_name': 'Brain',
                                 'locations': 'CA3', 'n_location': '3'}]
             self.assertEqual(result, expected_result)
-            
-    def test_get_samples_from_tsvfile(self):
-        try:
-            # Test case when tsvfile is None
-            ImageGeneNodeAttributeGenerator.get_samples_from_tsvfile()
-            self.fail('Expected exception')
-        except CellMapsDownloaderError as ce:
-            self.assertEqual('tsvfile is None', str(ce))
 
-        # Test case when file is not found
-        with self.assertRaises(FileNotFoundError):
-            ImageGeneNodeAttributeGenerator.get_samples_from_tsvfile('non_existent_file.tsv')
-
-        # Test case when tsvfile is empty
-        with patch('builtins.open', mock_open(read_data='')):
-            result = ImageGeneNodeAttributeGenerator.get_samples_from_tsvfile('test.tsv')
-            self.assertEqual(result, [])
-
-        # Test case when tsvfile has data
-        tsv_data = 'gene_names\tfile_link\tfile_name\nCDK5\thttps://example.com/cdk5_1.txt\tcdk5_1.txt\nGAD2\thttps://example.com/gad2_1.txt\tgad2_1.txt\nDLG4\thttps://example.com/dlg4_1.txt\tdlg4_1.txt\n'
-        with patch('builtins.open', mock_open(read_data=tsv_data)):
-            result = ImageGeneNodeAttributeGenerator.get_samples_from_tsvfile('test.tsv')
-            expected_result = [{'gene_names': 'CDK5', 'file_link': 'https://example.com/cdk5_1.txt', 'file_name': 'cdk5_1.txt'},
-                               {'gene_names': 'GAD2', 'file_link': 'https://example.com/gad2_1.txt', 'file_name': 'gad2_1.txt'},
-                               {'gene_names': 'DLG4', 'file_link': 'https://example.com/dlg4_1.txt', 'file_name': 'dlg4_1.txt'}]
-            self.assertEqual(result, expected_result)
-
-    def test_fun(self):
-        samples_list = ImageGeneNodeAttributeGenerator.get_samples_from_tsvfile(tsvfile='')
